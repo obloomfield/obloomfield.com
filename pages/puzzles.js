f9d57d032441c0e824a18270a4bd82333cd80cd9
obloomfield <obloomfield@brown.edu>
2021-11-28 21:59:06 -0500

    first round finalized

--- pages/puzzles.js
+++ pages/puzzles.js
@@ -1,41 +1,106 @@
-import { Container, Heading, SimpleGrid, Divider, Link} from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Divider, Link, Box, HStack, Button, Center, Badge} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-// import thumbFishWorkflow from '../public/images/contents/youtube-fish-workflow.jpg'
-// import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
-// import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
-// import thumbFinancialGoal from '../public/images/contents/blog-financial-goal.png'
-// import thumbHowToPriceYourself from '../public/images/contents/blog-how-to-price-yourself.jpg'
-// import thumb50xFaster from '../public/images/contents/youtube-50x-faster.jpg'
+import thumbPuz1 from '../public/images/links/eitherand3.png'
+import thumbPuz2 from '../public/images/links/dozen2.png'
+import thumbPuz3 from '../public/images/links/lost2.png'
+import thumbPuz4 from '../public/images/links/edible2.png'
+
+import {Meta} from '../components/work'
 
 const Puzzles = () => (
   <Layout title="Puzzles">
     <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Competiton Puzzles
-      </Heading>
-      In my free time, I like to write puzzles for these lovely competitions called <Link href='https://blog.vero.site/post/puzzlehunts' target='_blank'>Puzzle Hunts</Link>. 
-      These are word or logic puzzles where the underlying premise is that <i>you are not inherently given the instructions on how to solve a given puzzle, and the first (often hardest) step is making the leap towards guessing a mechanism. </i> 
-      <br></br>
-      Each puzzle has an underlying answer to extract, and they are structured in 'Meta' groups where going onto the next meta requires answering a 'metapuzzle,' which utilizes the answers of all the puzzles in that meta.
-      <br></br><br></br>
-      The format is very freeing; like escape rooms, any topic under the sun can become a puzzle. (Sudokus, Shakespeare encodings, alien communication attempts etc.) In equal parts, though, it is critical to define and make <i>motivated</i> puzzles, in which the solver feels comfortable teaching themselves the 'mechanics' at play. 
-      Deeper questions of motivation, teach-ability, and context, are what I find to be the most exciting aspect of this puzzle format, and are what inspire me to one day become a teacher.
-      <br></br><br></br>
-      I first found an interest for puzzles competing in my high school's <Link href='https://pea-puzzle-hunt.appspot.com/index.html' target='_blank'>hunt in 2017</Link>. My team did not get far at all, but we were glued to the content, and came back year after year, until my senior year when I volunteered to start writing for the event. 
-      Nowadays, I team up with my high school friends to tackle the yearly flagship online puzzle events with general success. 
-      <br></br><br></br>
-      <i>(My recent favorite hunts: <Link href='https://perpendicular.institute/' target='_blank'>MIT Mystery Hunt 2021</Link>, 
-      <Link href='https://www.umdpuzzle.club/puzzles' target='_blank'>UMD Puzzlehunt</Link>, <Link href='https://puzzlehunt.club.cc.cmu.edu/hunt/15/' target='_blank'>CMU Oregon Trail Hunt</Link>)
-      </i>
+      <Section delay={0.1}>
+        <Center>
+          <Heading as="h3" fontSize={20} mb={4}>
+            Highlighted Puzzles:
+          </Heading>
+        </Center>
+
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="http://www.peaph-archive.com/puzzles-by-year/2020/Summer%20Rabble%20Hunt/Meta%202/2-3.html?highlight=either"
+            title="Either And"
+            thumbnail={thumbPuz1}
+          >
+            <Badge mr={2} fontSize={10}>Summer 2020</Badge>
+            <br></br>
+            This puzzle is about pairing clue phrases together to describe an implied step between the two....
+            <br></br>
+            <Meta>Clue Phrases</Meta><Meta>Word Association</Meta>
+            
+            
+          </GridItem>
+          <GridItem
+            href="http://www.peaph-archive.com/puzzles-by-year/2021/Meta%204/4-5.html?highlight=dozen"
+            title="A Puzzle With the Answer of DOZEN"
+            thumbnail={thumbPuz2}
+          >
+            <Badge mr={2} fontSize={10}>Spring 2021</Badge>
+            <br></br>
+            This puzzle attempts to flip the normal solution paradigm on its head; giving you the answer 
+            immediately... unless?
+            <br></br>
+            <Meta>Logic</Meta><Meta>Word Association</Meta>
+          </GridItem>
+          <GridItem
+            href="http://www.peaph-archive.com/puzzles-by-year/2020/Spring%20Hunt/Meta%202/2-3.html"
+            title="Löst im Tranzläschon"
+            thumbnail={thumbPuz3}
+          >
+            <Badge mr={2} fontSize={10}>Spring 2020</Badge>
+            <br></br>
+            One of my first puzzles; this involves figuring out subsequent 'translations' of songs as an encoding scheme...
+            <br></br>
+            <Meta>Encoding</Meta><Meta>Logic</Meta><Meta>Song Trivia</Meta>
+          </GridItem>
+          <GridItem
+            href="http://www.peaph-archive.com/puzzles-by-year/2021/Meta%202/2-5.html?highlight=edible"
+            title="Edible Arrangements"
+            thumbnail={thumbPuz4}
+          >
+            <Badge mr={2} fontSize={10}>Spring 2021</Badge>
+            <br></br>
+            This puzzle is very similar to 'Either And,' but has a more flavorful twist...
+            <br></br>
+            <Meta>Clue Phrases</Meta><Meta>Word Association</Meta>
+          </GridItem>
+        </SimpleGrid>
+
+        <Box align="center" my={4} >
+          <HStack spacing="10px" justify="center">
+            <Link href="http://www.peaph-archive.com/search.html?q=Orion" target='_blank'>
+              <Button colorScheme="teal">
+                All My Puzzles
+              </Button>
+            </Link>
+
+          </HStack>
+        </Box>
+        
+
+      </Section>
+      
       <Section delay={0.2}>
         <Divider my={6} />
 
         <Heading as="h3" fontSize={20} mb={4}>
-          MORE CONTENT HERE COMING SOON
+          About
         </Heading>
+        In my free time, I like to write puzzles for these competitions called <Link href='https://blog.vero.site/post/puzzlehunts' target='_blank'>Puzzle Hunts</Link>. 
+      These are word or logic puzzles where the underlying premise is that <i>you are not inherently given the instructions on how to solve a puzzle, and the first (often hardest) step is making the leap towards guessing a "mechanism". </i> 
+      <br></br><br></br>
+      The format is very freeing; like escape rooms, any topic under the sun can become a puzzle. (Sudokus, Shakespeare encodings, alien communication attempts, etc.) In equal parts, though, it is critical to define and make <i>motivated</i> puzzles, in which the solver feels comfortable teaching themselves the 'mechanics' at play. 
+      Deeper questions of motivation, teach-ability, and context are what I find to be the most exciting aspect of puzzle writing. Harnessing these concepts has also excited me to one day become a teacher. :)
+      <br></br><br></br>
+      I first found an interest for puzzles competing in my high school's <Link href='https://pea-puzzle-hunt.appspot.com/index.html' target='_blank'>hunt in 2017</Link>. My team did not get far at all, but we were glued to the content. We participated year after year, until my senior year when I volunteered to start writing for the event. 
+      Nowadays, I team up with the local Providence team and my club to tackle the yearly flagship online puzzle events with general success. 
+      <br></br><br></br>
+      <i>(My recent favorite hunts: <Link href='https://perpendicular.institute/' target='_blank'>MIT Mystery Hunt 2021</Link>, 
+      <Link href='https://www.umdpuzzle.club/puzzles' target='_blank'>UMD Puzzlehunt</Link>, <Link href='https://puzzlehunt.club.cc.cmu.edu/hunt/15/' target='_blank'>CMU Oregon Trail Hunt</Link>)</i>
       </Section>
 
       {/* <Section delay={0.2}>
